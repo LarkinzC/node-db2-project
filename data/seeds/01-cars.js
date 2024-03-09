cf299d6d9cb43a9550ef4931c83d5360192dbf0a
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-09 13:13:23 -0800

    seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+    vin: '1829312789123',
+    make: 'toyota',
+    model: 'camry',
+    mileage: 18273,
+    title: 'clean',
+    transmission: 'automatic',
+},
+{
+    vin: '9032740232222',
+    make: 'chevy',
+    model: 'tahoe',
+    mileage: 13923,
+    title: 'clean',
+},
+{
+    vin: '0173517350933',
+    make: 'kia',
+    model: 'sorento',
+    mileage: 243242,
+},
+]
+
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() => {
+        return knex('cars').insert(cars)
+    })
+}
