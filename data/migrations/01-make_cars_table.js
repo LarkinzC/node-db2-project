c4f38f7b6ac4049d5ce2952e726dedbc031af396
Larkin Campbell <Larkincampbell24@gmail.com>
2024-03-09 13:17:34 -0800

    up

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,13 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+    return knex.schema.createTable('cars', tbl => {
+      tbl.increment('id')
+      tbl.string('vin', 17).notNullable().unique()
+      tbl.string('make', 20).notNullable()
+      tbl.string('model', 20).notNullable()
+      tbl.numeric('mileage').unsigned().notNullable()
+      tbl.string('title', 30)
+      tbl.string('transmision', 30)
+    })
 };
 
 exports.down = function (knex) {
